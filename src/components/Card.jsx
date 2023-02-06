4b415af55c54ede641d4a119c869e92239835bf4
SiriFazendoBarra <manuel.martinez.perezdearce@gmail.com>
2023-02-06 10:22:39 -0300

    2nd, avances

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,25 +1,26 @@
-export default function Card() {
+export default function Card(pizza) {
+  console.log(pizza)
   return (
     <article className="mb-2 col-12 col-md-6 col-xl-3">
       <div className="card">
         <img
-          src="https://firebasestorage.googleapis.com/v0/b/apis-varias-mias.appspot.com/o/pizzeria%2Fpizza-1239077_640_cl.jpg?alt=media&token=6a9a33da-5c00-49d4-9080-784dcc87ec2c"
+          src={pizza.item.img}
           className="card-img-top"
           alt="card description"
         />
         <div className="card-body">
           <h2>
-            <b>Pizza Napolitana</b>
+            <b>{pizza.item.name}</b>
           </h2>
           <h6>Ingrediente</h6>
           <ul>
-            <li>Mozzarella</li>
-            <li>Tomate</li>
-            <li>Orégano</li>
-            <li>Jamón</li>
+            {pizza.item.map((item)=> (
+              <li>{item}</li>
+          ))}
           </ul>
+          
           <h5>
-            <b>Precio: $5.950</b>
+            <b>Precio: ${pizza.item.price}</b>
           </h5>
           <div className="d-flex gap-2">
             <a
--- src/pages/Pizzas.jsx
+++ src/pages/Pizzas.jsx
@@ -1,13 +1,42 @@
+import { useState, useEffect } from "react";
 import Card from "../components/Card";
 
 export default function Pizzas() {
+
+  const [pizzas, setPizzas] = useState(null)
+  const [loading, setLoading] =useState(true)
+
+  const getPizzas = async () => {
+
+    try {
+      setLoading(true)
+      const res = await fetch('../../public/pizzas.json') 
+      const data = await res.json()
+      setPizzas(data)
+           
+    } catch (error) {
+      console.log(error)
+      
+    }
+    
+  }
+
+  useEffect(() => {
+    getPizzas()   
+  }, [])
+
+  if(!pizzas) return <h1>loading...</h1>
+   
+  console.log(pizzas)
   return (
     <div>
       <h1>Pizzas</h1>
       <div className="row">
-        {[1, 2, 3, 4].map((item) => (
-          <Card key={item} />
-        ))}
+        {
+        pizzas.map((item) =>{         
+        return(
+          <Card key={item.id} item={item} />
+        )})}
       </div>
     </div>
   );
